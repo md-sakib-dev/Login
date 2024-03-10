cb3dd2736858fb7a8a64421dec94dc6de22cf578
MD Sakib <mahbubalamsakib007@gmail.com>
2024-03-10 17:26:00 +0600

    Project

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/logo.jpg'
 import { Link } from "react-router-dom";
 import {useFormik} from 'formik'
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import pic from '../assets/pic.png'
 import { signInSchema } from '../schemas';
+import pic2 from '../assets/logo.png'
 const initialValues={
   email: "",
   password: "",
 };
 
-const Login = () => {   
+const Login = () => {  
+  const [visible,setVisible] =useState(false)
 const {values,touched, errors,handleBlur,handleChange,handleSubmit} = useFormik({
     initialValues:initialValues,
     validationSchema: signInSchema,
@@ -16,57 +21,71 @@ const {values,touched, errors,handleBlur,handleChange,handleSubmit} = useFormik(
 console.log(values);
     },
   });
-  console.log(errors);
+ 
  
   return (
+    <div className=' flex flex-col w-full h-screen'>
 
-    
-    <div className=' flex flex-col gap-2 md:justify-center md:items-center mt-16 w-[280px] md:w-[450px] ml-28 md:ml-44 sm:ml-60 lg:ml-[390px]'>
-        <div className='flex flex-col md:justify-center md:items-center shadow-md bg-slate-100 z-10 rounded-lg'>
-            <div className='ml-24 md:ml-6 '>
-            <img className='w-28' src={logo} alt="" />
+    <div className='flex flex-col justify-center items-center '>
+
+    <div className=' flex flex-col gap-2 md:justify-center  md:items-center  w-[280px] md:w-[450px] '>
+        <div className=' relative flex flex-col md:justify-center md:items-center shadow-lg bg-fuchsia-200 border border-slate-400 backdrop-filter backdrop-blur-sm bg-opacity-30  rounded-lg'>
+            <div className='ml-20 md:ml-6 '>
+            {/* <img className='w-28' src={pic} alt="" /> */}
+            <h1 className='text-2xl mt-5 font-bold  text-black'>artisanPOS</h1>
+            <span className="left-0 bottom-0 w-full h-1 bg-black"></span>
+
             </div>
-            {/* <div >
-                <h1 className='text-3xl font-medium ml-20 md:ml-8'>SIGN IN</h1>
-            </div> */}
+            <div >
+                <h1 className='text-lg mt-4 font-medium ml-20 md:ml-8'>Artisan Outfitters Limited</h1>
+            </div>
             <div>
                 <form className="flex flex-col gap-3 p-2 ml-2" onSubmit={handleSubmit}>
-                    <div className=' flex flex-col gap-3 '>
-                        <label htmlFor="email" className='text-2xl font-semibold'> 
+                    <div className=' flex flex-col gap-3 relative'>
+                        <input className=' bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500  focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer text-sm text-black  py-2.3 px-0 block rounded-md  w-[250px] md:w-[300px] h-10 ' type="email"  name='email' id='email' value={values.email} onChange={handleChange} onBlur={handleBlur} placeholder="" />
+                        <label htmlFor="email" className='absolute text-md text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 font-semibold'> 
                           Email
                         </label>
-                        <input className='bg-gray-50 border border-gray-300 text-gray-900 p-2.5 block rounded-md  w-[250px] md:w-[400px] h-10 ' type="email"  name='email' id='email' value={values.email} onChange={handleChange} onBlur={handleBlur} placeholder='Enter your Email'/>
+                        <div>
+
+                        <MdEmail size={20} className='absolute top-4 right-4'/>
+                        </div>
                         {errors.email && touched.email ? (
                 <p className=" text-red-700 font-semibold">{errors.email}</p>
               ) : null}
                     </div>
-                    <div className='flex flex-col gap-3'>
-                        <label htmlFor="password" className=' text-2xl font-semibold'>
+                    <div className='flex flex-col gap-3 relative'>
+                        <input className=" bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:focus:border-blue-500  focus:outline-none focus:ring-0 focus:text-black focus:border-blue-600 peer text-sm text-black  py-2.3 px-0 block rounded-md  w-[250px] md:w-[300px] h-10  " type={visible? "text" :"password"}  name="password" id="password" placeholder=''  value={values.password} onChange={handleChange} onBlur={handleBlur}/>
+                        <label htmlFor="password" className='absolute text-md text-black duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-800 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 font-semibold'>
                             Password
                         </label>
-                        <input className="bg-gray-50 border border-gray-300 text-gray-900 p-2.5 block rounded-md  w-[250px] md:w-[400px] h-10 " type="password"  name="password" id="password" placeholder='Enter your Password'  value={values.password} onChange={handleChange} onBlur={handleBlur}/>
+                        <div onClick={()=>setVisible(!visible)}>
+
+                       { visible ? <FaRegEye size={20} className='absolute top-4 right-4'/>:  <FaRegEyeSlash size={20} className='absolute top-4 right-4'/> }
+                        </div>
                         {errors.password && touched.password ? (
                 <p className=" text-red-700 font-semibold">{errors.password}</p>
               ) : null}
                 
                </div>
-               <div className=' flex justify-between'>
-                <div className='flex justify-center items-center gap-2'>
+               <div className=' flex justify-between '>
+                {/* <div className='flex justify-center items-center gap-2'>
                     <input  type="checkbox" name="right" id="right"/>
-                    <p className="text-md font-bold">Remember Me</p>
+                    <p className="text-sm sm:text-md font-bold">Remember Me</p>
 
                 </div>
+                <div>
+                <a to="" className=" text-sm sm:text-md  font-semibold mb-2 cursor-pointer text-black hover:text-blue-800">
+                  Forgot password?
+                </a>
+              </div> */}
                 
 
                </div>
-               <div className="flex justify-center bg-red-600 text-white rounded-md h-9 hover:opacity-90 hover:text-black hover:bg-emerald-500">
+               <div className="flex justify-center py-2 mb-5 bg-red-600 mt-2 text-white rounded-lg h-9 hover:opacity-90 hover:text-black hover:bg-emerald-500">
               <button className="">Login</button>
             </div>
-            <div>
-                <a to="" className="text-md text-blue-800 font-semibold mb-2 cursor-pointer">
-                  Forgot password?
-                </a>
-              </div>
+            
 
                 </form>
             </div>
@@ -77,6 +96,19 @@ console.log(values);
         </div>
 
     </div>
+    </div> 
+    <div className='h-1/10'>
+      
+      
+    <div className='border bg-white h-20 '>
+      <div className='flex  items-center'>
+        <img className=' w-20' src={pic2} alt='' />
+      </div>
+
+    </div>
+    </div>   
+    </div>
+
   )
 }
 
